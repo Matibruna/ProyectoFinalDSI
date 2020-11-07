7a649b8a2c0aa8db50199d8df5e250861d89a907
Matias Bruna <matibruna6@gmail.com>
2020-11-07 17:28:50 -0300

    Inicializando repositorio despliegue sistema, PPAI DSI 2020

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PedidosComponent } from './components/pedidos/pedidos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PedidosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
