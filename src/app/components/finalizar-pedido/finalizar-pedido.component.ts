d639bd238efa2749bad73fe45be57ed4d3f88f72
Matias Bruna <matibruna6@gmail.com>
2020-11-09 13:59:42 -0300

    Mejor que esto... dificil

--- src/app/components/finalizar-pedido/finalizar-pedido.component.ts
+++ src/app/components/finalizar-pedido/finalizar-pedido.component.ts
@@ -32,7 +32,8 @@ export class FinalizarPedidoComponent implements OnInit {
   }
 
   solicitarConfirmacionElaboracionProducto(){
-    this.mostrarTabla = false;
+    if(this.gestor.getDetallesPreConfirmados()==null || this.gestor.getDetallesPreConfirmados().length == 0){alert('Seleccione los pedidos que desea finalizar.')}
+    else{ this.mostrarTabla = false; }
   }
 
   cancelarConfirmacion(){
@@ -51,99 +52,107 @@ export class FinalizarPedidoComponent implements OnInit {
 
 
 
-  class GestorFinalizarPreparacionPedido{
+class GestorFinalizarPreparacionPedido{
 
-    private pedidos = Pedidos;
-    private detalles=this.buscarDetallesPedidoEnPreparacion();
-    seleccionados = [];
+  private pedidos = Pedidos;
+  private detalles=this.buscarDetallesPedidoEnPreparacion();
+  seleccionados: number[];
 
-    constructor(){
+  constructor(){
 
+  }
+
+  getDetalles(){
+    return this.detalles.sort(this.Comparator);
+  }
+
+  getDetallesPreConfirmados(){
+    if(this.seleccionados == null || this.seleccionados.length==0){return []}
+    let preSeleccionados: any[];
+    for(let n of this.seleccionados){
+      if(preSeleccionados == null){preSeleccionados = [this.detalles[n]]}
+      else{preSeleccionados.push(this.detalles[n])}
     }
-    getDetalles(){
-      return this.detalles.sort(this.Comparator);
+    return preSeleccionados;
     }
 
-    getDetallesPreConfirmados(){
-      let preSeleccionados: any[];
-      for(let n of this.seleccionados){
-        if(preSeleccionados == null){preSeleccionados = [this.detalles[n]]}
-        else{preSeleccionados.push(this.detalles[n])}
+  detalleDePedidoSeleccionado(i: number){
+    if(this.seleccionados == null){
+      this.seleccionados = [i]
+    }
+    else{
+      let x = this.estaSeleccionado(i);
+      if(x != -1){ 
+        console.log(this.seleccionados)
+          this.seleccionados.splice(x, 1); 
+        console.log(this.seleccionados)
       }
-      return preSeleccionados;
-    }
-
-    detalleDePedidoSeleccionado(i: number){
-      if(this.seleccionados == null){
-        this.seleccionados = [i]
-      }
-      else{
-        let x = this.estaSeleccionado(i);
-        if(x >= 0){ this.seleccionados = this.seleccionados.filter( item => item!=x ); }
-        else { this.seleccionados.push(i)}
-      }
-    }
-
-    confirmacionElaboracion(){
-      this.actualizarEstadoDetallePedido();
-    }
-
-    actualizarEstadoDetallePedido(){
-      for(let n of this.seleccionados){
-        this.detalles[n].Detalle.finalizar();
-        this.detalles[n].Detalle.notificar();
+      else{ 
+        this.seleccionados.push(i)
       }
     }
+  }
 
-    ordenarSegunMayorTiempoDeEspera(){
-      this.detalles.sort(this.Comparator) 
+  confirmacionElaboracion(){
+    this.actualizarEstadoDetallePedido();
+  }
+
+  actualizarEstadoDetallePedido(){
+    for(let n of this.seleccionados){
+      this.detalles[n].Detalle.finalizar();
+      this.detalles[n].Detalle.notificar();
     }
+  }
 
-    buscarDetallesPedidoEnPreparacion(){
-      let actualDate = new Date();
-      let mesa : string;
-      let retornoDetalles: any[];
-      let ped: Pedido;
+  ordenarSegunMayorTiempoDeEspera(){
+    this.detalles.sort(this.Comparator) 
+  }
+
+  buscarDetallesPedidoEnPreparacion(){
+    let actualDate = new Date();
+    let mesa : string;
+    let retornoDetalles: any[];
+    let ped: Pedido;
   
-      for(ped of this.pedidos){
-        mesa = ped.getMesaToString();
-        let detalle: DetalleDePedido;
-        for (detalle of ped.getDetalles()){
-          if(detalle.estaEnPreparacion()){
-            var tiempo = (actualDate.getTime() - detalle.getfechaHora().getTime())
-            if(retornoDetalles == null)
-            {
-              retornoDetalles = [{
-                Mesa: mesa,
-                Nom: detalle.getNombreProducto(),
-                Cant: detalle.getCantidad(),
-                Date: Math.round(((tiempo/1000)/60)),
-                Detalle: detalle 
-                }]
-            }
-            else
-              {
-                retornoDetalles.push({Mesa: mesa, Nom: detalle.getNombreProducto(), Cant: detalle.getCantidad(), Date: Math.round(((tiempo/1000)/60)), Detalle: detalle})
-              }
+    for(ped of this.pedidos){
+      mesa = ped.getMesaToString();
+      let detalle: DetalleDePedido;
+      for (detalle of ped.getDetalles()){
+        if(detalle.estaEnPreparacion()){
+          var tiempo = (actualDate.getTime() - detalle.getfechaHora().getTime())
+          if(retornoDetalles == null)
+          {
+            retornoDetalles = [{
+              Mesa: mesa,
+              Nom: detalle.getNombreProducto(),
+              Cant: detalle.getCantidad(),
+              Date: Math.round(((tiempo/1000)/60)),
+              Detalle: detalle 
+              }]
+          }
+          else
+          {
+            retornoDetalles.push({Mesa: mesa, Nom: detalle.getNombreProducto(), Cant: detalle.getCantidad(), Date: Math.round(((tiempo/1000)/60)), Detalle: detalle})
           }
         }
       }
-      return retornoDetalles;
     }
-
-    Comparator(a, b) {
-      if (a.Date > b.Date) return -1;
-      if (a.Date < b.Date) return 1;
-      return 0;
-    }
-
-    estaSeleccionado(n: number){
-      for(let i=0; i<this.seleccionados.length; i++){
-        if(this.seleccionados[i] == n){return i;}
-      }
-      return -1;
-    }
+    return retornoDetalles;
   }
+
+  Comparator(a, b) {
+    if (a.Date > b.Date) return -1;
+    if (a.Date < b.Date) return 1;
+    return 0;
+  }
+
+  estaSeleccionado(n: number){
+    for(let i=0; i<this.seleccionados.length; i++){
+      if(this.seleccionados[i] == n){return i;}
+    }
+    return -1;
+  }
+}
 
 
 
